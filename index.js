46ad727ad6e389be52f04a11f57c88d294eb8dcd
Sujit Akulwar <sujitakulwar@gmail.com>
2022-07-18 20:28:58 +0530

    10

---
+++ index.js
@@ -0,0 +1,15 @@
+let button00 = document.getElementById('00');
+let button01 = document.getElementById('01');
+let button02 = document.getElementById('02');
+let button10 = document.getElementById('10');
+let button11 = document.getElementById('11');
+let button12 = document.getElementById('12');
+let button20 = document.getElementById('20');
+let button21 = document.getElementById('21');
+let button22 = document.getElementById('22');
+let val;
+function fun(val){
+    if(val == '?'){
+        
+    }
+}
