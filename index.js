b07349c0877825c0f72d05ab7c436d103e6a4e7f
Sujit Akulwar <sujitakulwar@gmail.com>
2022-07-18 20:54:44 +0530

    12

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-
 let counter = 0;
-function fun(val){
-    if(document.getElementById(val) == '?'){
+function fun00(){
+    var val = document.getElementById('00').value; 
+    if(val == '?'){
         if(counter % 2 == 0){
-            document.getElementById(val).innerHTML = 'X';
+            document.getElementById('00').innerHTML = 'X';
         }
         else{
-            document.getElementById(val).innerHTML = '0';
+            document.getElementById('00').innerHTML = '0';
         }
         counter++;
     }
